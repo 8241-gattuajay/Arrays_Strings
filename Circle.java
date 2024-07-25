80564bbc9acf991c82b6e9e94077bdd53fc74c4c
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-07-25 21:55:20 +0530

    Circle Area and Perimeter

---
+++ Circle.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Circle {
+    public static void main(String[] args) {
+        int r=5;
+        double area=Math.PI*r*r;
+        double perimeter=2*Math.PI*r;
+        System.out.println("Area of Circle is :"+area);
+        System.out.println("Perimeter of Circle is :"+perimeter);
+    }
+}
